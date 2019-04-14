ae716e1a394397ebcf88510574069ee3717ba444
Joe T. Wilson <jtwilson.home@gmail.com>
2019-04-14 00:37:48 -0600

    added dot product and transpose to Matrix

--- src/app/App.java
+++ src/app/App.java
@@ -1,24 +1,33 @@
 package app;
 
+import java.util.Arrays;
+
 import processing.core.PApplet;
 
-public class App extends PApplet{
+public class App extends PApplet {
     NeuralNet net;
     Matrix mat;
-    public static void main(String[] args)  {
-        PApplet.main("app.App");    //must match this file name and package
+
+    public static void main(String[] args) {
+        PApplet.main("app.App"); // must match this file name and package
     }
 
-    public void settings(){         //only used for setting the size of the canvas
-        size (640, 480);
+    public void settings() { // only used for setting the size of the canvas
+        size(640, 480);
     }
 
-    public void setup(){            //runs once on launch
-        net=new NeuralNet(3, 3, 1);
-        
+    public void setup() { // runs once on launch
+        net = new NeuralNet(3, 3, 1);
+        mat = new Matrix(2, 3);
+        mat.randomizeInt();
+        Matrix mat2 = new Matrix(3, 1);
+        mat2.randomizeInt();
+        System.out.println(Arrays.deepToString(mat.matrix));
+        Matrix mat3 = Matrix.transpose(mat);
+        System.out.println(Arrays.deepToString(mat3.matrix));
     }
 
-    public void draw(){             //main loop
+    public void draw() { // main loop
 
     }
 }
--- src/app/Matrix.java
+++ src/app/Matrix.java
@@ -2,27 +2,88 @@ package app;
 
 public class Matrix {
     int rows, cols;
-    float[][] matrix;
+    double[][] matrix;
 
-    public Matrix(int rows_, int cols_){
-        rows=rows_;
-        cols=cols_;
+    public Matrix(int rows_, int cols_) {
+        rows = rows_;
+        cols = cols_;
 
-        matrix = new float[rows][cols];
+        matrix = new double[rows][cols];
     }
 
-    public void scMult(float n){
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                matrix[i][j]*=n;
+    public void scMult(double n) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] *= n;
             }
         }
     }
 
-    public void scAdd(float n){
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                matrix[i][j]+=n;
+    public void scAdd(double n) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] += n;
+            }
+        }
+    }
+
+    public void elemAdd(Matrix m) {
+        if ((this.rows == m.rows) && (this.cols == m.cols)) {
+            for (int i = 0; i < this.rows; i++) {
+                for (int j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] += m.matrix[i][j];
+                }
+            }
+        } else {
+            System.err.println("attempting to add two matrices of different size");
+        }
+    }
+
+    public void elemMult(Matrix m) {
+        if ((this.rows == m.rows) && (this.cols == m.cols)) {
+            for (int i = 0; i < this.rows; i++) {
+                for (int j = 0; j < this.cols; j++) {
+                    this.matrix[i][j] *= m.matrix[i][j];
+                }
+            }
+        } else {
+            System.err.println("attempting to multiply two matrices of different size");
+        }
+    }
+
+    public static Matrix product(Matrix a, Matrix b) {
+        Matrix result = new Matrix(a.rows, b.cols);
+        if (a.cols == b.rows) {
+            for (int i = 0; i < result.rows; i++) {
+                for (int j = 0; j < result.cols; j++) {
+                    double sum = 0;
+                    for (int k = 0; k < a.cols; k++) {
+                        sum += a.matrix[i][k] * b.matrix[k][j];
+                    }
+                    result.matrix[i][j] = sum;
+                }
+            }
+        } else {
+            System.err.println("attempting to perform matrix products on bad sizes");
+        }
+        return result;
+    }
+
+    public static Matrix transpose(Matrix a) {
+        Matrix result = new Matrix(a.cols, a.rows);
+        for (int i = 0; i < result.rows; i++) {
+            for (int j = 0; j < result.cols; j++) {
+                result.matrix[i][j] = a.matrix[j][i];
+            }
+        }
+        return result;
+    }
+
+    public void randomizeInt() {
+
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.cols; j++) {
+                this.matrix[i][j] = Math.floor((Math.random() - 0.5) * 20);
             }
         }
     }
