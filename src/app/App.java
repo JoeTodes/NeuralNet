5d5d586ffe45a250620464f083f0bf33f7b217ab
JoeTodes <jtwilson.home@gmail.com>
2019-04-16 15:38:44 -0600

    visualized XOR training

--- src/app/App.java
+++ src/app/App.java
@@ -5,7 +5,8 @@ import processing.core.PApplet;
 public class App extends PApplet {
     double[][] inputArr = { { 1, 0 }, { 1, 1 }, { 0, 0 }, { 0, 1 } };
     double[][] targetArr = { { 1 }, { 0 }, { 0 }, { 1 } };
-    NeuralNet net;
+    NeuralNet nn;
+    int iterations;
 
     public static void main(String[] args) {
         PApplet.main("app.App"); // must match this file name and package
@@ -18,22 +19,33 @@ public class App extends PApplet {
     public void setup() { // runs once on launch
         // net = new NeuralNet(2, 2, 1, 0.3);
         int[] config = { 2, 6, 1 };
-        NeuralNet nn = new NeuralNet(config, 0.1);
+        nn = new NeuralNet(config, 0.1);
         // nn.feedforward(this.inputArr[0]);
-
-        for (int j = 0; j < 100000; j++) {
-            int index = floor(random(0, 4));
-            nn.train(this.inputArr[index], this.targetArr[index]);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            nn.feedforward(inputArr[i]);
-            nn.results[2].print();
-        }
 
     }
 
     public void draw() { // main loop
+        for (int j = 0; j < 100; j++) {
+            int index = floor(random(0, 4));
+            nn.train(this.inputArr[index], this.targetArr[index]);
+            iterations += 1;
+        }
 
+        loadPixels();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                pixels[i + width * j] = color(51);
+                double mappedX = map(i, 0, this.width, 0, 1);
+                double mappedY = map(j, 0, this.height, 0, 1);
+                double[] pix = { mappedX, mappedY };
+                double[] result = nn.guess(pix);
+                pixels[i + (width * j)] = color(floor(map((float) result[0], 0, 1, 0, 255)));
+            }
+        }
+        updatePixels();
+
+        textSize(50);
+        fill(255);
+        text(iterations, 0, 50);
     }
 }
--- src/app/NeuralNet.java
+++ src/app/NeuralNet.java
@@ -1,31 +1,9 @@
 package app;
 
 public class NeuralNet {
-    int hiddenLayers, totalLayers, inputNodes, hiddenNodes, outputNodes;
+    int hiddenLayers, totalLayers;
     double learningRate;
     Matrix[] weights, biases, results, errors, gradients, deltaWeights;
-
-    // notes for refactoring for arbitrary #of hiddens:
-    // accept as input an array with node numbers
-    // make a Matrix[] len=inputarr.length
-    // make a Matrix[] of weights, len=inputarr.length-1
-    // make a Matrix[] of the biases, length = inputarr.length-1
-    public NeuralNet(int numIn, int numHidden, int numOut, double learningRate) {
-        /*
-         * this.inputNodes = numIn; this.hiddenNodes = numHidden; this.outputNodes =
-         * numOut; this.learningRate = learningRate;
-         * 
-         * this.weightsIH = new Matrix(this.hiddenNodes, this.inputNodes);
-         * this.weightsHO = new Matrix(this.outputNodes, this.hiddenNodes);
-         * 
-         * this.weightsIH.randomizeDouble(-1, 1); this.weightsHO.randomizeDouble(-1, 1);
-         * 
-         * this.biasH = new Matrix(this.hiddenNodes, 1); this.biasO = new
-         * Matrix(this.outputNodes, 1); this.biasH.randomizeDouble(-1, 1);
-         * this.biasO.randomizeDouble(-1, 1);
-         */
-
-    }
 
     public NeuralNet(int[] layers, double learningRate) {
         this.totalLayers = layers.length;
@@ -49,11 +27,7 @@ public class NeuralNet {
         }
     }
 
-    // notes for refactoring for arbitrary #of hiddens:
-    // inputs Matrix in nodesArr[0]
-    // iterate through to nodesArr.length-2 doing the feedforward
     public void feedforward(double[] input) {
-        // inputs = Matrix.fromArray(input);
         this.results[0] = Matrix.fromArray(input);
 
         for (int i = 0; i < totalLayers - 1; i++) {
@@ -61,18 +35,6 @@ public class NeuralNet {
             this.results[i + 1].elemAdd(this.biases[i]);
             this.results[i + 1].map(x -> sigmoid(x));
         }
-
-        // return this.results[this.totalLayers - 1].toArray();
-        /*
-         * hiddens = Matrix.product(this.weightsIH, inputs); // matrix multiply weights
-         * with inputs hiddens.elemAdd(this.biasH); // add in bias hiddens.map(x ->
-         * sigmoid(x)); // activation function
-         * 
-         * outputs = Matrix.product(this.weightsHO, hiddens);
-         * outputs.elemAdd(this.biasO); outputs.map(x -> sigmoid(x));
-         * 
-         * return outputs.toArray();
-         */
     }
 
     double sigmoid(double in) {
@@ -83,11 +45,7 @@ public class NeuralNet {
         return y * (1 - y);
     }
 
-    // notes for refactoring for arbitrary #of hiddens:
-    // after calculating output error, loop backwards through nodes
     public void train(double[] inputs, double[] targets) {
-        // Matrix outputs = Matrix.fromArray(feedforward(inputs));
-        // Matrix answers = Matrix.fromArray(targets);
 
         feedforward(inputs);
         Matrix answers = Matrix.fromArray(targets);
@@ -105,28 +63,13 @@ public class NeuralNet {
                 this.errors[i - 1] = Matrix.product(Matrix.transpose(this.weights[i]), errors[i]);
             }
         }
+    }
 
-        /*
-         * // calculate output error and adjust weightsHO and biases Matrix outErrors =
-         * Matrix.elemSub(answers, outputs); Matrix outGradients = Matrix.map(outputs, x
-         * -> unSigmoid(x)); outGradients.elemMult(outErrors); outGradients.map(x -> x *
-         * this.learningRate); Matrix dWeightsHO = Matrix.product(outGradients,
-         * Matrix.transpose(hiddens)); this.weightsHO.elemAdd(dWeightsHO);
-         * this.biasO.elemAdd(outGradients);
-         * 
-         * // calculate hidden layer errors and adjust weightsIH and biases Matrix
-         * hiddenErrors = Matrix.product(Matrix.transpose(this.weightsHO), outErrors);
-         * Matrix hiddenGradients = Matrix.map(hiddens, x -> unSigmoid(x));
-         * hiddenGradients.elemMult(hiddenErrors); hiddenGradients.map(x -> x *
-         * this.learningRate); Matrix dWeightsIH = Matrix.product(hiddenGradients,
-         * Matrix.transpose(this.inputs)); this.weightsIH.elemAdd(dWeightsIH);
-         * this.biasH.elemAdd(hiddenGradients);
-         */
-        // calculate input layer errors
-        // Matrix inputErrors = Matrix.product(Matrix.transpose(this.weightsIH),
-        // hiddenErrors);
+    public double[] guess(double[] inputs_) {
+        double[] result;
+        feedforward(inputs_);
+        result = this.results[totalLayers - 1].toArray();
 
-        // outErrors.print();
-        // System.out.println("done train");
+        return result;
     }
 }
